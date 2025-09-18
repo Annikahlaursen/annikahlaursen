37eb3342f3f3e52604ff05d25a7d48cd169b5c1d
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-09-18 15:08:15 +0200

    pages ændringer

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  // Change base path when building for production
+  if (command !== "serve") {
+    config.base = "/annikahlaursen/"; // 👈 Replace with your GitHub repository name
+  }
+
+  return config;
 });
