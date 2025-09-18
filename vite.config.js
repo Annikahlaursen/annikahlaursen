6a073b50646e110cf1c069184a0e17dea168d002
Annikahlaursen <Annikahlaursen@gmail.com>
2025-09-18 14:15:38 +0200

    hej

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/annikahlaursen/"; // 👈 Replace with your repository name
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
 });
